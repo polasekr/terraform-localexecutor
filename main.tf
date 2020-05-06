f4838b003627641888032fbd575d6040813a2bcb
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 12:14:20 -0400

    fix

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "null_resource" "pwd" {
 
   provisioner "local-exec" {
-    command = "pwd &> pwd.out ; echo "RESULT: $?" >> pwd.out"
+    command = "pwd &> pwd.out ; echo \"RESULT: $?\" >> pwd.out"
   }
 }
 
 resource "null_resource" "ls" {
 
   provisioner "local-exec" {
-    command = "ls -la &> ls.out ; echo "RESULT: $?" >> ls.out"
+    command = "ls -la &> ls.out ; echo \"RESULT: $?\" >> ls.out"
   }
 }
 
