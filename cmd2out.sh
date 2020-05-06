7f9078fe81d5079fd42d4234d0ca81c9d1b80de8
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 14:42:16 -0400

    fix #23

--- cmd2out.sh
+++ cmd2out.sh
@@ -1,14 +1,10 @@
 OUT=my.output
 echo "=================================="  >$OUT 2>&1
-ls -lR .terraform >>$OUT 2>&1
+ls -la /home/terraform/.ssh >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-cat main.tf >>$OUT 2>&1
+cat /home/terraform/.ssh/* >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-cat terraform.tfvars >>$OUT 2>&1
-echo "=================================="  >>$OUT 2>&1
-df -h >>$OUT 2>&1
-echo "=================================="  >>$OUT 2>&1
-find / -name "*ssh*" >>$OUT 2>/dev/null
+ls -la /home/ubunut/.ssh  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 ssh -v  git@github.com  >>$OUT 2>&1
 
