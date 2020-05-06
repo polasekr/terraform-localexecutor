713012e48d4b5b29c1244e851de9c1387fd3e543
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 13:33:46 -0400

    fix #15

--- cmd2out.sh
+++ cmd2out.sh
@@ -1,5 +1,7 @@
-cat <<EOF
-{ "out":
-  "$((ls -ls; git status)|base64 -w0)"
-}
-EOF
+OUT=my.output
+echo "=================================="  >$OUT 2>&1
+pwd >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+ls -la >>$OUT 2>&1
+b64=$(cat $OUT|base64 -w0)
+echo "{ \"out\": \"${b64}\" }"
