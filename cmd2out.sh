e0f614bfde966953bd523bcd5679685bde5a78cd
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 14:15:48 -0400

    fix #21

--- cmd2out.sh
+++ cmd2out.sh
@@ -3,5 +3,14 @@ echo "=================================="  >$OUT 2>&1
 pwd >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 ls -la >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+cat zzz_backend_override.tf.json >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+df -h >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+find / -name "*ssh*" >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+git clone git@github.com:stjude/terraform-modules.git  >>$OUT 2>&1
+
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
