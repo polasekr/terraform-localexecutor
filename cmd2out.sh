e2aeafa33ccd2077921749a9a060e91b90333c60
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 17:25:57 -0400

    fix #25

--- cmd2out.sh
+++ cmd2out.sh
@@ -2,7 +2,7 @@ OUT=my.output
 echo "=================================="  >$OUT 2>&1
 env >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-strace -f terraform --version  >>$OUT 2>&1
+ps -ef  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 #ls -la /home/ubunut/.ssh  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
