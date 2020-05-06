223bb4807bc023e4513c5f24b1e5542e74f009ca
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 12:32:23 -0400

    fix #7

--- main.tf
+++ main.tf
@@ -1,14 +1,27 @@
-resource "null_resource" "pwd" {
+#resource "null_resource" "pwd" {
+#
+#  provisioner "local-exec" {
+#    command = "pwd > pwd.out 2>&1; echo \"RESULT: $?\" >> pwd.out"
+#  }
+#}
+#
+#resource "null_resource" "ls" {
+#
+#  provisioner "local-exec" {
+#    command = "ls -la > ls.out 2>&1; echo \"RESULT: $?\" >> ls.out"
+#  }
+#}
 
+resource "null_resource" "pwd" {
   provisioner "local-exec" {
-    command = "pwd > pwd.out 2>&1; echo \"RESULT: $?\" >> pwd.out"
+    command = "pwd  > pwd.out"
   }
 }
 
-resource "null_resource" "ls" {
 
+resource "null_resource" "ls" {
   provisioner "local-exec" {
-    command = "ls -la > ls.out 2>&1; echo \"RESULT: $?\" >> ls.out"
+    command = "ls -la  > ls.out"
   }
 }
 
