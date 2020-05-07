3f3235d415edbbdb7a68d40a53a9703b91a61385
Robert Polasek <robert.polasek@toptal.com>
2020-05-07 11:02:31 -0400

    fix #33

--- cmd2out.sh
+++ cmd2out.sh
@@ -2,11 +2,11 @@ OUT=my.output
 echo "=================================="  >$OUT 2>&1
 env >>$OUT 2>&1
 echo "=================================="  >>$OUT 2>&1
-ps -ef  >>$OUT 2>&1
-echo "=================================="  >>$OUT 2>&1
-id  >>$OUT 2>&1
-echo "=================================="  >>$OUT 2>&1
 cat /tmp/.ssh_private_key   >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+git clone 'git@github.com:stjude/terraform-modules.git'  >>$OUT 2>&1
+echo "=================================="  >>$OUT 2>&1
+ps -ef  >>$OUT 2>&1
 
 b64=$(cat $OUT|base64 -w0)
 echo "{ \"out\": \"${b64}\" }"
